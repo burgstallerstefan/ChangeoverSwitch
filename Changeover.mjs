08d930711de2fecc53dfd6518438d98cb3932e6b
burgstallerstefan <123748992+burgstallerstefan@users.noreply.github.com>
2025-07-23 21:32:08 +0200

    Update Changeover.mjs

--- Changeover.mjs
+++ Changeover.mjs
@@ -1,4 +1,37 @@
+let cyclicTime = 10; // ms
+
+/*##################  EVENT PIPELINE #########################*/
+let eventPipeline = []; // Array für Events
+let eventPipelineStart = 0; // Startindex der Warteschlange
+let exeEventPipe = false;
+
+function addEventToPipeline(event) {
+  eventPipeline[eventPipeline.length] = event;
+}
+
+function getNextEventFromPipeline() {
+  if (eventPipelineStart < eventPipeline.length) {
+    let event = eventPipeline[eventPipelineStart]; 
+    eventPipelineStart++;
+    return event;
+  }else {
+    eventPipelineStart = 0; 
+    eventPipeline = []; 
+  }
+  return null;      
+}
+
+function executeEventPipeline() {
+  let event = getNextEventFromPipeline();
+  if (event) {
+    handler(event);
+    return false;
+  }
+  return true; // keine Events mehr in der Warteschlange
+}
+
 /*###################  LOGGING  #########################*/
+const EXCEPTION = -1;
 const VERBOSE = 0;
 const INFO = 1;
 const WARNING = 2;
@@ -23,7 +56,7 @@ function logger(level, message) {
 /*##################  MODEL  #########################*/
 let shelly = { 
   crossed: false,
-  load: false,
+  load: true,
   input: {
       "0": {
         state: false
@@ -34,22 +67,63 @@ let shelly = {
   },
   output: {
       "0": {
-        state: false,
+        state: false
       },
       "1": {
-        state: false,
+        state: false
       }
   }
 };
 
 let prevShelly = JSON.parse(JSON.stringify(shelly));
 
-let outputRPC = {
-call:[true, true],
-done:[false, false]
-};
+function model_OutputChanged(){
+  return prevShelly.output !== shelly.output;
+}
 
-let inputRPC = {
+function model_InputChanged(){
+  return prevShelly.input !== shelly.input;
+}
+
+function model_LoadChanged(){
+  return prevShelly.load !== shelly.load;
+}
+
+function model_IsLoad(){
+  return shelly.load;
+}
+
+function model_IsCrossed(){
+  return shelly.crossed;
+}
+
+function model_ToggleCrossed(){
+  shelly.crossed = !shelly.crossed;
+}
+
+function model_bothOutputs(state){ 
+  return (shelly.output["0"].state === state && shelly.output["1"].state === state);
+}
+
+function model_bothInputs(state){
+  return (shelly.input["0"].state === state && shelly.input["1"].state === state);
+} 
+
+function model_SetOutputs(){
+  // No intermediate states allowed
+  if(!model_bothInputs(false) && !model_bothInputs(true)){
+    if(! model_IsCrossed()){
+      shelly.output["0"].state = shelly.input["0"].state;
+      shelly.output["1"].state = shelly.input["1"].state;
+    }else{
+      shelly.output["0"].state = shelly.input["1"].state;
+      shelly.output["1"].state = shelly.input["0"].state;
+    }
+  }
+}
+
+/*##################  RPC CALLS  #########################*/
+let outputRPC = {
 call:[true, true],
 done:[false, false]
 };
@@ -62,7 +136,7 @@ let _isSwitch = false;
 let _state;
 let _apower = 0.0;
 
-Shelly.addEventHandler(function(event, _) {
+function handler(event){
   function handleToggleEvent(isInput, id, state) {
     if(isInput) { shelly.input[id].state = state; }
     else { shelly.output[id].state = state; }
@@ -70,12 +144,13 @@ Shelly.addEventHandler(function(event, _) {
   function handlePowerUpdateEvent(id, apower) {
     shelly.load = (apower > 0.0);
   }
-  logger(VERBOSE, "Event: " + JSON.stringify(event));
+
   _id = event.component;
   _input = _id.slice(0, _id.indexOf(':'));
   _isInput = _input === "input";
   _isSwitch = !_isInput;
   _id = _id.slice(_id.indexOf(':')+1);
+
   if (event.info && typeof event.info === "object" && "event" in event.info ) {
     if (event.info.event === "toggle" && "state" in event.info && typeof event.info.state === "boolean") {
       _state = event.info.state;
@@ -89,32 +164,40 @@ Shelly.addEventHandler(function(event, _) {
       logger(ERROR, "Unknown event type: " + event.info.event);
       return;
     }
-    logger(INFO, _input)
-    logger(INFO, _id)
-    logger(INFO, _state)
+    logger(INFO, "Event handled: " + _input + ":" + _id + " - State: " + _state);
 
     changeover();
   }
+}
+
+Shelly.addEventHandler(function(event, _) {
+  logger(INFO, "Event received: " + JSON.stringify(event));
+  addEventToPipeline(event);
 }, null);
+Shelly.addStatusHandler(function(event, _) {
+  /*logger(INFO, "Status received: " + JSON.stringify(event));
+  addEventToPipeline(event);*/
+}, null);
+
 
 /*################## CHANGEOVER  #########################*/
 function changeover(){
-  function crosslogic(){
-    
-  }
   logger(WARNING, "     INPUT 0  :  "+shelly.input["1"].state + "     INPUT 1  :  "+shelly.input["0"].state);
   logger(WARNING, "     OUTPUT 0 :  "+shelly.output["1"].state + "     OUTPUT 1 :  "+shelly.output["0"].state);
-
-  crosslogic()
-
-  if(! shelly.crossed){
-    shelly.output["0"].state = shelly.input["0"].state;
-    shelly.output["1"].state = shelly.input["1"].state;
-  }else{
-    shelly.output["0"].state = shelly.input["1"].state;
-    shelly.output["1"].state = shelly.input["0"].state;
+  logger(WARNING, "              LOAD      :  "+model_IsLoad());
+  
+  // Startup
+  if(model_bothInputs(false) && model_bothOutputs(true)){
+    logger(INFO, "!TODO!");
   }
-  prevShelly = JSON.parse(JSON.stringify(shelly));
+
+  // Alexa logic
+  if(model_LoadChanged() && model_OutputChanged()){
+    model_ToggleCrossed();
+  }
+  
+
+  model_SetOutputs();
   writeOutputs();
 }
 
@@ -151,134 +234,53 @@ function writeOutputs(){
   return false;
 }
 
-/*##################  READ INPUTS  #########################*/
-function readInputs(){
-  if(inputRPC.done[0] && inputRPC.done[1]){
-    //logger(VERBOSE, "Both Input-RPC-Call done.");
-    inputRPC.call[0] = true;
-    inputRPC.done[0] = false;
-    inputRPC.call[1] = true;
-    inputRPC.done[1] = false;
-    return true;
-  }
-
-  if(inputRPC.call[0]){
-    inputRPC.call[0] = false;
-    Shelly.call("Input.GetStatus", {id: 0}, function(result) {
-        if(result){
-          inputRPC.done[0] = true;
-          if(result.state === null){
-            result.state = false;
-          }
-          shelly.input["0"].state = result.state;
-          logger(VERBOSE, "Input-RPC-Call [0] done.");
-        }
-    });
-  }
-
-  if(inputRPC.call[1]){
-    inputRPC.call[1] = false;
-    Shelly.call("Input.GetStatus", {id: 1}, function(result) {
-        if(result){
-          inputRPC.done[1] = true;
-          if(result.state === null){
-            result.state = false;
-          }
-          shelly.input["1"].state = result.state;
-          logger(VERBOSE, "Input-RPC-Call [1] done.");
-        }
-    });
-  }
-
-  return false;
-}
-
-/*##################  GET STATUS  #########################*/
-function getStatus(){
-  if(inputRPC.done[0] && inputRPC.done[1]){
-    //logger(VERBOSE, "Both Input-RPC-Call done.");
-    inputRPC.call[0] = true;
-    inputRPC.done[0] = false;
-    inputRPC.call[1] = true;
-    inputRPC.done[1] = false;
-    return true;
-  }
-
-  if(inputRPC.call[0]){
-    inputRPC.call[0] = false;
-    Shelly.call("Input.GetStatus", {id: 0}, function(result) {
-        if(result){
-          inputRPC.done[0] = true;
-          if(result.state === null){
-            result.state = false;
-          }
-          shelly.input["0"].state = result.state;
-          logger(VERBOSE, "Input-RPC-Call [0] done.");
-        }
-    });
-  }
-
-  if(inputRPC.call[1]){
-    inputRPC.call[1] = false;
-    Shelly.call("Input.GetStatus", {id: 1}, function(result) {
-        if(result){
-          inputRPC.done[1] = true;
-          if(result.state === null){
-            result.state = false;
-          }
-          shelly.input["1"].state = result.state;
-          logger(VERBOSE, "Input-RPC-Call [1] done.");
-        }
-    });
-  }
-
-  return false;
-}
-
 /*##################  INIT SHELLY  #########################*/
-function initShelly() {
-
-  Shelly.call("Switch.SetConfig", {
+Shelly.call("Switch.SetConfig", {
+id: 0,
+config: {
   id: 0,
-  config: {
-    id: 0,
-    name: "Switch 0",
-    in_mode: "detached",
-    initial_state: "off",
-    in_locked: false,
-    auto_on: false,
-    auto_on_delay: 60,
-    auto_off: false,
-    auto_off_delay: 60,
-    autorecover_voltage_errors: false,
-    power_limit: 4480,
-    voltage_limit: 280,
-    undervoltage_limit: 0,
-    current_limit: 16
-  },
-  });   
+  name: "Switch 0",
+  in_mode: "detached",
+  initial_state: "off",
+  in_locked: false,
+  auto_on: false,
+  auto_on_delay: 60,
+  auto_off: false,
+  auto_off_delay: 60,
+  autorecover_voltage_errors: false,
+  power_limit: 4480,
+  voltage_limit: 280,
+  undervoltage_limit: 0,
+  current_limit: 16
+},
+});   
 
-  Shelly.call("Switch.SetConfig", {
-    id: 1,
-  config: {
-    id: 1,
-    name: "Switch 1",
-    in_mode: "detached",
-    initial_state: "off",
-    in_locked: false,
-    auto_on: false,
-    auto_on_delay: 60,
-    auto_off: false,
-    auto_off_delay: 60,
-    autorecover_voltage_errors: false,
-    power_limit: 4480,
-    voltage_limit: 280,
-    undervoltage_limit: 0,
-    current_limit: 16
-  },
-  });   
+Shelly.call("Switch.SetConfig", {
+  id: 1,
+config: {
+  id: 1,
+  name: "Switch 1",
+  in_mode: "detached",
+  initial_state: "off",
+  in_locked: false,
+  auto_on: false,
+  auto_on_delay: 60,
+  auto_off: false,
+  auto_off_delay: 60,
+  autorecover_voltage_errors: false,
+  power_limit: 4480,
+  voltage_limit: 280,
+  undervoltage_limit: 0,
+  current_limit: 16
+},
+});
 
-  changeover();
+function cyclic(){
+  exeEventPipe = !executeEventPipeline();
+  if(!exeEventPipe){
+    logger(VERBOSE, "ExecuteEventPipeline successful.");
+  }
 }
-/*##################  INIT SHELLY  #########################*/
-initShelly();
+
+
+Timer.set(cyclicTime, true, cyclic);
